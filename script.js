02cbc5bf3a5bcfa4e4b0e568a9df7f08560f32e8
hawraalattar <hanovalattar@gmail.com>
2024-05-08 12:36:29 +0300

    checkWin function works

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ let columns = 3
 let referencePuz
 let swapPuz
 
-const finalImgOrder = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+const finalImgOrder = [
+  'http://localhost:5500/puzzle/1.jpg',
+  'http://localhost:5500/puzzle/2.jpg',
+  'http://localhost:5500/puzzle/3.jpg',
+  'http://localhost:5500/puzzle/4.jpg',
+  'http://localhost:5500/puzzle/5.jpg',
+  'http://localhost:5500/puzzle/6.jpg',
+  'http://localhost:5500/puzzle/7.jpg',
+  'http://localhost:5500/puzzle/8.jpg',
+  'http://localhost:5500/puzzle/9.jpg'
+]
 let puzzleOrder = ['4', '6', '2', '8', '5', '3', '1', '9', '7']
 
 // Loading the puzzle board
@@ -27,6 +37,7 @@ for (let r = 0; r < rows; r++) {
     puzzle.addEventListener('drop', dropPuz)
     puzzle.addEventListener('dragend', dragEnd)
 
+    console.log(checkWin())
     // Append
     document.getElementById('board').append(puzzle)
   }
@@ -62,12 +73,19 @@ function dragEnd() {
 
 // Function to check win
 function checkWin() {
-  let playerBoard = document.getElementById('board')
+  let playerBoard = document.getElementById('board').children
   let playerOrder = []
   for (let i = 0; i < playerBoard.length; i++) {
-    playerOrder.push(playerBoard[i])
-    console.log(playerOrder)
+    playerOrder.push(playerBoard[i].src)
   }
+  console.log(playerOrder)
+  // for loop
+  for (let index = 0; index < playerOrder.length; index++) {
+    // if two elements are not equal change value of returnValue to false
+    if (playerOrder[index] !== finalImgOrder[index]) {
+      return false
+    }
+    // end of for loop return returnValue
+  }
+  return true
 }
-
-checkWin()
